77a9e10ca80dbc25b8bfe1082c6821349cc8fa8d
Nikit0sin <nikitabaranuk10316@gmail.com>
2022-12-18 19:51:17 +0300

    1 Семестр проект

--- PZ_10/PZ_10_1.py
+++ PZ_10/PZ_10_1.py
@@ -1,3 +1,13 @@
-a = {'молоко', 'соль', 'сахар'}
-b = {'мясо', 'молоко', 'сыр'}
-print(a)
+magnit = {"shop": "Магнит",
+          "products": {"молоко", "соль", "сахар"}}
+pyaterochka = {"shop": "Пятёрочка",
+             "products": {"мясо", "молоко", "сыр"}}
+
+allProducts = magnit["products"].union(pyaterochka["products"])
+difmagnit = allProducts - magnit["products"]
+difpyaterohcka = allProducts - pyaterochka["products"]
+print("Товары из Магнита, которые отсутствуют в Пятёрочке: ", difpyaterohcka)
+print("Товары из Пятёрочки, которые отсутствуют в Магните: ", difmagnit)
+print("Перечень всех продуктов: ", allProducts)
+print("Из данных перечней товаров в магазинах равно(ы) только:",
+      magnit["products"] & pyaterochka["products"])
