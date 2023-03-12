55ebb3a5e56373e5344ca0d191c71d5776ac45e0
Nikit0sin <nikitabaranuk10316@gmail.com>
2023-03-12 18:28:53 +0300

    1 Семестр проект

---
+++ PZ_14/PZ_14_1.py
@@ -0,0 +1,20 @@
+import re
+
+openf = open('writer.txt', encoding='UTF-8')
+read = openf.read()
+openf.close()
+
+surname = re.findall(r"[^\S](\b[А-Я]\w+)\s[А-Я].[А-Я].", read)
+surname_surname = re.findall(r"(\b[А-Я-]\w+.[А-Я]\w*)\s[А-Я].[А-Я].", read)
+
+read_roman = re.sub(r"роман\b", 'произведение', read)
+
+print("Фамилии:", surname)
+print("Фамилии через дефис:", surname_surname)
+print("Количество фамилий:", len(surname))
+print("Количество фамилий через дефис:", len(surname_surname))
+
+final = open('final.txt', 'w')
+final.writelines(read_roman)
+final.close()
+
